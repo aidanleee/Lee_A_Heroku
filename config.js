eea2a50e89d78300d0d0b5678b1c00eacaa53bcd
Aidan Lee <Aidan@Aidans-MacBook-Pro.local>
2019-03-22 20:25:29 -0400

    add home

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    pword: "root", // windows leave this blank
+    host: "localhost",
+    port: "8889", // windos users - sql port is 3306
+    database: "macbook"
+}
+
+module.exports = config;
